d6f9acc7587d75b866049fd93f1f303028e779e0
pablo romero <romeropablojr03@gmail.com>
2026-07-27 15:14:17 -0500

    perf

--- init.lua
+++ init.lua
@@ -1,4 +1,8 @@
 vim.env.NVIM_LOG_FILE = "/dev/null"
+if vim.loader then
+	vim.loader.enable()
+end
+
 require("config.controls")
 require("config.options")
 require("config.lazy")
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,4 +12,25 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+	change_detection = {
+		notify = false,
+	},
+	performance = {
+		cache = {
+			enabled = true,
+		},
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+})
