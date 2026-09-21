7395c26f8bc81b330915525a54819b4a5fdf56a5
pablo romero <romeropablojr03@gmail.com>
2026-09-21 12:49:52 -0500

    sync

--- lua/config/controls.lua
+++ lua/config/controls.lua
@@ -9,6 +9,7 @@ end
 
 map("n", "<Leader>w", "<cmd>write<CR>", "Write buffer")
 map("n", "<Leader>q", "<cmd>quit<CR>", "Quit window")
+map("n", "<C-]>", "%", "Jump to matching pair")
 
 -- Use the system clipboard without changing unnamed registers.
 map("v", "<C-c>", '"+y', "Copy to system clipboard")
